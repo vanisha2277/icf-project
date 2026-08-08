070ebabd0a6aafb7856eb82a7f9c42649a03ff47
Acacia Edmond <aedmond@macalester.edu>
2026-08-08 15:57:41 -0500

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage/LandingPage";
 import Preferences from "./components/Preferences/preferences";
 import ReadingMode from "./components/ReadingMode/ReadingMode";
+import Vocabulary from "./components/Vocabulary/vocabulary";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<LandingPage />} />
       <Route path="/preferences" element={<Preferences />} />
       <Route path="/reading-mode" element={<ReadingMode />} />
+      <Route path="/vocabulary" element={<Vocabulary />} />
     </Routes>
   );
 }
